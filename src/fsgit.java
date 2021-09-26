9fba28a8afea371b369198d86ed2e13e716d30c3
Felipe AM <molinamanuelfelipe@gmail.com>
2021-09-25 20:24:23 -0500

    Subs Method

--- src/fsgit.java
+++ src/fsgit.java
@@ -3,11 +3,16 @@ public class fsgit {
 
         System.out.println("Hello World!");
         System.out.println(add(1,2));
+        System.out.println(subs(1,2));
 
     }
 
     public static int add (int a, int b ){
         return a + b;
+    }
+
+    public static int subs (int a, int b ){
+        return a - b;
     }
 
 }
